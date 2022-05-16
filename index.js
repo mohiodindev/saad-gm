b94a914337db470552761e6fd5dcb28625a201a7
Ali Saad <alisaad@dynamiclogix.com>
2022-05-16 12:18:11 +0500

    test

--- index.js
+++ index.js
@@ -1,16 +1,1 @@
-<<<<<<< HEAD
-jhkjklgjkleqwjhkwerjhketejhktejhkr3jhkqerjhktjhkerjhkgss
-
-new data
-jhdhdjfjdjhkddjhjhfjhkejhefjhdjhwe
-// gmmmmmmmmmmmm
-//table of 5 in javascript
-jkljhldjhlfjljklfsdjlfgjlrhgsafjkltrhjlgwejhlawejkrejkge
-fjhlkdfjhkdfjhsdfjhjhsjhfj 
-
-=======
-aKSDJHKHKJJHCB,ASJSB,J,DJHLJHLEJHWEJHJHWJHKJJHKLDJHJHDJH
-JHLDJHKLDJHDJHLJHDSJHDJHSDSDCWDJHKLCDJCDJHCE
-;JKSJKLDSDJHLCDJHDJKHWVJHKW
-JHLDCJHDHKJKLVJHL VBJCLXPODPOF
->>>>>>> saad
+console.log("test")
